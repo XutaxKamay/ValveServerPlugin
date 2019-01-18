3c79e285bcabc5ca011cc6f044dbb4fa4679156b
Kamay Xutax <xutaxkamay@gmail.com>
2019-01-19 00:49:23 +0100

    Fixed issues with runtime linking

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-gppflags='-Wall -std=c++17 -shared -m32 -fPIC -Ofast -s -static'
+gppflags='-Wall -shared -m32 -fPIC -Ofast -s -static -std=c++0x'
 time(
 	echo "Compiling"
 	g++ $gppflags main.cpp -o sample_plugin.so
